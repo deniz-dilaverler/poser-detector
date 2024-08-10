d6d8d6db27f6e81e87f81324f701c42ad0272d79
Deniz Mert Dilaverler <dmertdilaverler@gmail.com>
2024-08-10 18:55:34 +0300

    Add a basic footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,29 @@
+import "../styles/Footer.css"
+
+
+export default function Footer() {
+
+  return (
+    <footer>
+      <div className="footer-child">
+        <div className="footer-content">
+          <h2>Customer Service</h2>
+          <p>About Payment</p>
+          <p>Returns Policy</p>
+        </div>
+      </div>
+      <div className="footer-child-center">
+        <div className="footer-content">
+          <h2 className="hardcore">Hardcore</h2>
+        </div>
+      </div>
+      <div className="footer-child">
+        <div className="footer-content">
+          <h2>About Company</h2>
+          <p>Our Values</p>
+          <p>0% Officially Licensed</p>
+        </div>
+      </div>
+    </footer>
+  )
+}
