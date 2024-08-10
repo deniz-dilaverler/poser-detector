c5b30afbdab03f76a76a4b2441641bb7f35a6aa7
CerenAkyr <cerenakyar@gmail.com>
2024-08-10 13:40:26 +0300

    Colors and Titles Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="main-title">This is the main title</h1>
+      <h2 className="artist-title">This is the artist title (secondary title)</h2>
+        <p className="description">This is the description</p>
     </>
   )
 }
