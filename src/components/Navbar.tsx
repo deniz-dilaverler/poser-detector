268c88f7a664ccad8e4f4c5e25a8d2a33caea888
CerenAkyr <cerenakyar@gmail.com>
2024-08-10 14:56:39 +0300

    Dimension Limitation Removed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,17 +3,18 @@ import cart from '../assets/shopping-cart.png';
 import '../styles/Navbar.css';
 const Navbar = () => {
   return (
-    <nav>
-      <div className="logo-container">
-          <img src={logo} alt=""/>
-          <h1 className="main-title">HARDCORE</h1>
-      </div>
-        <div>
-            <button className="shopping-cart-button">
-                <img src={cart} alt=""/>
-            </button>
-        </div>
-    </nav>
+
+        <nav>
+            <div className="logo-container">
+                <img src={logo} alt=""/>
+                <h1 className="main-title">HARDCORE</h1>
+            </div>
+            <div>
+                <button className="shopping-cart-button">
+                    <img src={cart} alt=""/>
+                </button>
+            </div>
+        </nav>
   );
 };
 
